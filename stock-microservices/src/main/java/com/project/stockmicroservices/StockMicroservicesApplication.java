a8db7325a119c39fd906911f29ed42d67462589f
ngqhung0912 <67213674+ngqhung0912@users.noreply.github.com>
2023-03-21 16:11:38 +0100

    first commit

---
+++ stock-microservices/src/main/java/com/project/stockmicroservices/StockMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.project.stockmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockMicroservicesApplication.class, args);
+	}
+
+}
---
+++ stock-microservices/src/test/java/com/project/stockmicroservices/StockMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.stockmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
