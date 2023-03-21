3452e52b2a64cfa824c0e8a585de6840203af151
EpsilonWu <wuyueming0912@outlook.com>
2023-03-21 16:55:15 +0100

    [ADD] initialize barcode-reader service"

---
+++ barcode-reader/src/main/java/com/project/barcodereader/BarcodeReaderApplication.java
@@ -0,0 +1,13 @@
+package com.project.barcodereader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarcodeReaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarcodeReaderApplication.class, args);
+    }
+
+}
