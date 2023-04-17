ac451e6097495865dc398aae59466772778c42f9
EpsilonWu <wuyueming0912@outlook.com>
2023-04-17 16:59:09 +0200

    [ADD]saga

--- payment/src/main/java/com/project/payment/service/PaymentService.java
+++ payment/src/main/java/com/project/payment/service/PaymentService.java
@@ -2,12 +2,19 @@ package com.project.payment.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.payment.entity.PaymentLog;
 import com.project.payment.entity.PaymentResponse;
+import com.project.payment.repository.PaymentRepository;
+import jakarta.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
@@ -21,9 +28,18 @@ public class PaymentService {
   @Autowired
   private ObjectMapper objectMapper;
 
+  @Autowired
+  private PaymentRepository paymentRepository;
+
   private Logger logger = LoggerFactory.getLogger(PaymentService.class);
 
+  @Transactional
   public boolean pay() {
+    PaymentLog log = new PaymentLog();
+    log.setUuid(UUID.randomUUID().toString());
+    log.setEventId(0);
+    log.setStatus("created");
+    paymentRepository.save(log);
     RestTemplate restTemplate = new RestTemplate();
     Double amount = 0.0;
     String userId = "1";
@@ -40,7 +56,7 @@ public class PaymentService {
     logger.info("Payment proceed with amount: " + amount);
 
     String message = null;
-    PaymentResponse response = new PaymentResponse(1L, true);
+    PaymentResponse response = new PaymentResponse(1L, true, log.getUuid());
     try {
       message = objectMapper.writeValueAsString(response);
     } catch (JsonProcessingException e) {
@@ -48,5 +64,63 @@ public class PaymentService {
     }
     kafkaTemplate.send("PAYMENT", message);
     return true;
+  }
+
+  @KafkaListener(topics = "PAYMENT_LOG")
+  @Transactional
+  public void monitor(String message) {
+    try {
+      PaymentLog paymentLog = objectMapper.readValue(message, PaymentLog.class);
+      List<PaymentLog> paymentLogs = paymentRepository.getAllByUuid(paymentLog.getUuid());
+      boolean rollback = false;
+      PaymentLog initialLog = null;
+      List<PaymentLog> logEvents = new ArrayList<>();
+      for (PaymentLog log: paymentLogs) {
+        if (log.getEventId() == 0) {
+          initialLog = log;
+          if ("Error".equals(log.getStatus())) {
+            rollback = true;
+          }
+        } else {
+          logEvents.add(log);
+        }
+      }
+      if (rollback) {
+        sendRollBackMessage(paymentLog);
+        return;
+      }
+      paymentRepository.save(paymentLog);
+      if ("Error".equals(paymentLog.getStatus())) {
+        initialLog.setStatus("Error");
+        paymentRepository.save(initialLog);
+        batchRollBack(logEvents);
+      }
+    } catch (Exception e) {
+      logger.error(e.getMessage());
+    }
+  }
+
+  private void sendRollBackMessage(PaymentLog paymentLog) {
+    try {
+      paymentLog.setStatus("Error");
+      String rollbackMessage = objectMapper.writeValueAsString(paymentLog);
+      kafkaTemplate.send("PAYMENT_ROLLBACK"+paymentLog.getEventId(), rollbackMessage);
+      paymentRepository.save(paymentLog);
+    } catch (JsonProcessingException e) {
+      logger.error(e.getMessage());
+    }
+  }
+
+  private void batchRollBack(List<PaymentLog> paymentLogs) {
+    try{
+      for (PaymentLog paymentLog: paymentLogs) {
+        paymentLog.setStatus("Error");
+        String rollbackMessage = objectMapper.writeValueAsString(paymentLog);
+        kafkaTemplate.send("PAYMENT_ROLLBACK"+paymentLog.getEventId(), rollbackMessage);
+        paymentRepository.save(paymentLog);
+      }
+    } catch (Exception e) {
+
+    }
   }
 }
