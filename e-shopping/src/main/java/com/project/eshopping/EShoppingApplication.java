94c91a344326a4bbf5496389df5f78afae046d95
EpsilonWu <wuyueming0912@outlook.com>
2023-04-18 10:31:11 +0200

    fix minor things

--- e-shopping/src/main/java/com/project/eshopping/EShoppingApplication.java
+++ e-shopping/src/main/java/com/project/eshopping/EShoppingApplication.java
@@ -2,9 +2,7 @@ package com.project.eshopping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 public class EShoppingApplication {
 
